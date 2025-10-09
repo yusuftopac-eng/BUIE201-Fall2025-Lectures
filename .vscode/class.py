667a7f8aae09c8f0ea32a601f112fb39e6a8991c
Yusuf Topaç <ysftpc81@gmail.com>
2025-10-09 23:04:47 +0300

    commit1

---
+++ .vscode/class.py
@@ -0,0 +1,14 @@
+#new namespace created "Student"
+class Student:
+    grade = 0.0
+    def Study():
+        print("I am studying")
+
+tamer = Student()
+tamer.grade = 4.0
+ayse = Student()
+ayse.grade = 3.0
+fatma = Student()
+fatma.grade = 2.0
+
+print(tamer.grade)
